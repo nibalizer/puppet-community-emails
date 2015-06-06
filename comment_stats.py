e0747dc7b2b95154c8c4be90158cc92ef06aca2b
Spencer Krum <nibz@spencerkrum.com>
2015-06-06 10:50:28 -0700

    Table view, better perf

--- comment_stats.py
+++ comment_stats.py
@@ -5,6 +5,7 @@ from github import Github
 from datetime import datetime, timedelta
 import config
 
+from pdb import set_trace
 from collections import defaultdict
 
 def totimestamp(dt, epoch=datetime(1970,1,1)):
@@ -12,6 +13,38 @@ def totimestamp(dt, epoch=datetime(1970,1,1)):
     # return td.total_seconds()
     return int((td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 1e6 )
 
+def before(date, days=30):
+    if date < (datetime.now() - timedelta(30)):
+        return False
+    else:
+        return True
+
+user_actions = {}
+
+def user_action(user, action):
+    if user is None:
+        return
+    if user not in user_actions.keys():
+        user_actions[user] = {
+                'closed': 0,
+                'merged': 0,
+                'commented': 0,
+                'reopened': 0,
+                'opened': 0 }
+    else:
+        user_actions[user][action] += 1
+
+
+def pp_user_actions():
+    print "User                  |Opened|Closed|Merged|Commented|ReOpened"
+    for user in sorted(user_actions.keys()):
+        print "{:<22}|{:>6}|{:>6}|{:>6}|{:>9}|{:>8}".format(
+                user,
+                user_actions[user]['opened'],
+                user_actions[user]['closed'],
+                user_actions[user]['merged'],
+                user_actions[user]['commented'],
+                user_actions[user]['reopened'])
 
 if __name__ == "__main__":
 
@@ -19,33 +52,23 @@ if __name__ == "__main__":
     pc = g.get_organization('puppet-community')
 
     user_actions = {}
-    for repo in pc.get_repos():
+    total_issues_created = 0
+    print "Warning not running on full dataset"
+    for repo in pc.get_repos()[:10]:
         #from pdb import set_trace; set_trace()
         print repo.name
-        for event in repo.get_issues_events():
-            try:
-                username = event.actor.login
-            except:
-                continue
+        for issue in repo.get_issues(state='all'):
 
-            #from pdb import set_trace; set_trace()
-            if event.created_at < (datetime.now() - timedelta(30)):
-                continue
-
-            print username, event.created_at, event.event
-            if event.event in ['closed', 'merged', 'reopened', 'opened']:
-                print username, "did", event.event
-                if username not in user_actions.keys():
-                    user_actions[username] = {
-                            'closed': 0,
-                            'merged': 0,
-                            'reopened': 0,
-                            'opened': 0 }
-                else:
-                    user_actions[username][event.event] += 1
+            # Handle creation
+            if before(issue.created_at):
+                user_action(issue.user.login, 'opened')
+                total_issues_created += 1
 
 
-    print user_actions
+
+
+    pp_user_actions()
+    print "Total Issues Created(last 30 days): {0}".format(total_issues_created)
 
 
 
